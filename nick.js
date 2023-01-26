c2f0ed9328f8015cbc8364cbc3bd84a7b52c5ad1
chulander <bryan.p.chu@gmail.com>
2023-01-26 13:34:09 -0500

    clear interval

--- nick.js
+++ nick.js
@@ -1,40 +1,29 @@
-function bomb(x, y) {
-  inputArr.push({ x, y });
-  console.log("bomb", x, y);
+const coordinates = {
+  "00": true,
+  "01": true,
+  "02": true,
+  10: true,
+  11: true,
+  12: true,
+  20: true,
+  21: true,
+  22: true,
+};
+const generateMath = (count = 1) => Math.floor(Math.random() * count);
+
+let interval = setInterval(bomber, 1000);
+
+function bomber() {
+  const keys = Object.keys(coordinates);
+  if (keys.length) {
+    const num = generateMath(keys.length);
+    const [x, y] = keys[num];
+    console.log("bomb", x, y);
+    // remove from existing
+    delete coordinates[keys[num]];
+  } else {
+    clearInterval(interval);
+  }
 }
 
-const inputArr = [];
-function isBombed(x, y) {
-  let exist = false;
-  inputArr.forEach((item) => {
-    if (item.x === x && item.y === y) {
-      exist = true;
-    }
-  });
-  return exist;
-}
-
-function generateMath() {
-  return Math.floor(Math.random() * 3);
-}
-
-let interval = setInterval(broker, 1000);
-function broker() {
-  // [0, 0], [0, 1], [0, 2][(1, 0)], [1, 1], [1, 2][(2, 0)], [2, 1], [2, 2];
-  let iterate = true;
-  do {
-    let x = generateMath();
-    let y = generateMath();
-    if (!isBombed(x, y)) {
-      bomb(x, y);
-    }
-    // check if all been bomb
-    if (inputArr.length >= 9) {
-      clearInterval(interval);
-    }
-
-    // check if its all been bombed
-  } while (iterate);
-}
-
-broker();
+bomber();
